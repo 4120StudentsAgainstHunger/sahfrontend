d36f86927b4e20ef2dd7f3f82ca3add3c37329bb
JeffLechner <77597882+JeffLechner@users.noreply.github.com>
2021-05-08 09:16:22 -0500

    Connectivity fix Attempt 4

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -9,28 +9,28 @@ export class APIService {
 
    getBlogPage(param_pk) {
      const url = `${API_URL}/sahbackend/blogpages/${param_pk}`;
-     return axios.get(url, {headers: headers});
+     return axios.get(url);
   }
 
    getBlogPageList() {
     const url = `${API_URL}/sahbackend/blogpages/`;
-    return axios.get(url, {headers: headers});
+    return axios.get(url);
 
   }
 
   addNewFoodRequest(foodrequest){
   const url = `${API_URL}/sahbackend/foodrequests/`;
-  return axios.post(url, foodrequest, {headers: headers});
+  return axios.post(url);
  }
 
   getEvent(param_pk) {
     const url = `${API_URL}/sahbackend/events/${param_pk}`;
-    return axios.get(url, {headers: headers});
+    return axios.get(url);
   }
 
   getEventList() {
       const url = `${API_URL}/sahbackend/events/`;
-      return axios.get(url, {headers: headers});
+      return axios.get(url);
 
   }
 }
